f2300864c4ba81bba9db8ab44b208a5f4c19ba78
Maks Klimashevskiy <38065721+klimasevskiy@users.noreply.github.com>
2022-06-07 09:23:00 +0300

    Add files via upload

---
+++ lab8/lab8/Point.cs
@@ -0,0 +1,29 @@
+﻿/*Створити клас точка, що має координати. Визначити класи еліпсів і кіл.
+Визначити ієрархію типів. Визначити функції виведення, конструктори, деструктори,
+обчислення площі.*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab8
+{
+    internal class Point
+    {
+        private double x;
+        private double y;
+
+        public Point() { this.x = 0; this.y = 0; }
+        public Point(double a, double b) { this.x = a; this.y = b; }
+        public Point(Point p) { this.x = p.x; this.y = p.y; }
+        public double GetX() { return this.x; }
+        public double GetY() { return this.y; }
+        public void SetX(double value) { this.x = value; }
+        public void SetY(double value) { this.y = value; }
+        public void Display()
+        {
+            Console.WriteLine($"Point({x};{y})");
+        }
+    }
+}
---
+++ lab8/lab8/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Створити клас точка, що має координати. Визначити класи еліпсів і кіл.
+Визначити ієрархію типів. Визначити функції виведення, конструктори, деструктори,
+обчислення площі.*/
+namespace lab8
+{
+    internal class Program
+    {
+        public static void Main(String[] args)
+        {
+            Circle circle = new Circle(1, 3, 5);
+            Elipse elipse = new Elipse(1, 1, 6, 3);
+            circle.CircleDisplay();
+            elipse.ElipseDisplay();
+        }
+    }
+}
